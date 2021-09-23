650def55a9b76a64d0db18d07c386b0df62ff7ab
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-09-23 10:15:01 -0700

    Add sidebar and header to project creation form

--- frontend/components/logged_in/project_creation/project_creation_form.jsx
+++ frontend/components/logged_in/project_creation/project_creation_form.jsx
@@ -54,30 +54,36 @@ class ProjectCreationForm extends React.Component {
   render(){
     // debugger
     return (
-      <div className="auth-form-container">
-        <form onSubmit={this.handleSubmit} 
-              className="auth-form">
-          <div className="auth-inputs">
-            <span className="auth-input-label">Project Name</span>
-            <input type="text"
-                    value={this.state.name}
-                    onChange={this.update('name')}
-                    className="auth-input-field"
-            />
-            <span className="auth-input-label">Team</span>
-            <select onChange={this.update('team_id')}
-                    className="auth-input-field"
-            >
-              {
-                this.state.teams.map(team => <option value={team.id} key={team.id}>{team.name}</option>)
-              }
-            </ select>
+      <div className="home-main">
+        {this.props.sidebar}
+        <div>
+          {this.props.header}
+          <div className="auth-form-container">
+            <form onSubmit={this.handleSubmit} 
+                  className="auth-form">
+              <div className="auth-inputs">
+                <span className="auth-input-label">Project Name</span>
+                <input type="text"
+                        value={this.state.name}
+                        onChange={this.update('name')}
+                        className="auth-input-field"
+                />
+                <span className="auth-input-label">Team</span>
+                <select onChange={this.update('team_id')}
+                        className="auth-input-field"
+                >
+                  {
+                    this.state.teams.map(team => <option value={team.id} key={team.id}>{team.name}</option>)
+                  }
+                </ select>
+              </div>
+              <input type="submit" 
+                      value="Create Project"
+                      className="auth-form-submit"
+              />
+            </form>
           </div>
-          <input type="submit" 
-                  value="Create Project"
-                  className="auth-form-submit"
-          />
-        </form>
+        </div>
       </div>
     );
   }
--- frontend/components/logged_in/project_creation/project_creation_form_container.js
+++ frontend/components/logged_in/project_creation/project_creation_form_container.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
+import HomeHeaderContainer from '../header/home_header_container'
+import SidebarContainer from '../sidebar/sidebar_container';
+
 import { createProject } from '../../../actions/project_actions';
 import { getTeams } from '../../../actions/team_actions';
 import ProjectCreationForm from './project_creation_form';
 const mapStateToProps = ({ entities }) => {
-  return { teams: entities.teams };
+  return { 
+    teams: entities.teams,
+    header: <HomeHeaderContainer title="Team Show Page" />,
+    sidebar: <SidebarContainer /> 
+  };
 };
 
 const mapDispatchToProps = dispatch => {
