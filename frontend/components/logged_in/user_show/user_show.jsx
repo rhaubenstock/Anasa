78b204499707e6760fe2be5f0db3d3e273e94eab
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-11-04 23:15:41 -0700

    Add description to user page

--- frontend/components/logged_in/user_show/user_show.jsx
+++ frontend/components/logged_in/user_show/user_show.jsx
@@ -4,11 +4,35 @@ import { Link } from 'react-router-dom';
 class UserShow extends React.Component{
   constructor(props){
     super(props);
+    this.state = { description: this.props.description || "",
+                 };
+    this.saveDescription = this.saveDescription.bind(this);
   }
 
+  saveDescription(){
+    
+  }
+
+  changeDescription(){
+    return (e) => {
+      this.setState({ description: e.currentTarget.value });
+      // still having an error of leaving off the last character, 
+      // but debouncing should fix that, see below:
+      //
+      // https://davidwalsh.name/javascript-debounce-function
+      this.saveDescription();
+    };
+  }
   componentDidMount(){};
   render(){
-    return ;
+    return (
+      <div>
+        <textarea onChange={this.changeDescription}
+                  value={this.state.description}
+        />
+      </div>
+      
+    );
   }
 }
 export default UserShow;
