cd035d3aa057a2620990b20ca5002ea4812af0d6
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2022-02-01 21:05:25 -0800

    Add mui drawer

--- frontend/components/logged_in/sidebar/sidebar.jsx
+++ frontend/components/logged_in/sidebar/sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import TeamCreateShowContainer from './team_create/team_create_form_container';
+import Drawer from '@material-ui/core/Typography'
 
 class Sidebar extends React.Component {
   constructor(props){
@@ -13,23 +14,31 @@ class Sidebar extends React.Component {
 
   render(){
     return (
-      <div className="home-sidebar">
-        <div className="home-sidebar-header">
-            <Link to="/home" className="home-logo-container">
-              <div className="home-logo">
-                <h5>anasa</h5>
-              </div>
-            </Link>
+      <Drawer
+        variant="temporary"
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+
+        <div className="home-sidebar">
+          <div className="home-sidebar-header">
+              <Link to="/home" className="home-logo-container">
+                <div className="home-logo">
+                  <h5>anasa</h5>
+                </div>
+              </Link>
+          </div>
+          <div className="home-sidebar-teams">
+            {this.props.teams.map( team => (
+              <Link to={`/teams/${team.id}`} key={team.id}>
+                <span>{team.name}</span>
+              </Link>
+            ))}
+          </div>
+          <TeamCreateShowContainer />
         </div>
-        <div className="home-sidebar-teams">
-          {this.props.teams.map( team => (
-            <Link to={`/teams/${team.id}`} key={team.id}>
-              <span>{team.name}</span>
-            </Link>
-          ))}
-        </div>
-        <TeamCreateShowContainer />
-      </div>
+      </Drawer>
     )
   }
 };
