8ca449f12fa3caf2456ff20391bc2b40d7f4b931
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2022-02-03 20:28:52 -0800

    Add drawer header

--- frontend/components/logged_in/sidebar/sidebar.jsx
+++ frontend/components/logged_in/sidebar/sidebar.jsx
@@ -13,6 +13,16 @@ class Sidebar extends React.Component {
   }
 
   render(){
+    // reference: https://mui.com/components/drawers/#PersistentDrawerRight.js
+    const DrawerHeader = styled('div')(({ theme }) => ({
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    }));
+
     return (
       <Drawer
         variant="temporary"
