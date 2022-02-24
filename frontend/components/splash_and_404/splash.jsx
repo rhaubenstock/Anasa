013563bf9c8de8dc6af1d873dc5d9faba8ab4632
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2022-02-23 23:08:13 -0800

    Work on splash page content

--- frontend/components/splash_and_404/splash.jsx
+++ frontend/components/splash_and_404/splash.jsx
@@ -3,19 +3,32 @@ import Header from './header';
 import Footer from './footer';
 
 export default ({ signupLink }) => {
+      // <div className="splash-main">
+      //   <div className="splash-text">
+      //     {/* {<h3>Work on big ideas, without the busywork.</h3>
+      //     <p>From the small stuff to the big picture, Asana organizes work so teams know what to do, why it matters, and how to get it done.</p>} */}
+      //     {/* {signupLink} */}
+      //   </div>
+      //   <div className="splash-img">
+      //    {/* { <img src="assets/teamwork.jpeg" alt="teamwork!"/>} */}
+      //   </div>
+      // </div>
   return (
     <>
       <Header />
-      <div className="splash-main">
-        <div className="splash-text">
-          {/* {<h3>Work on big ideas, without the busywork.</h3>
-          <p>From the small stuff to the big picture, Asana organizes work so teams know what to do, why it matters, and how to get it done.</p>} */}
-          {/* {signupLink} */}
+      <section className="splashSection">
+        <div className="splashSection-container">
+          <div className="">
+            <div className="">
+              <div className="splashSection-mainText-Container">
+                <h1 className="splashSection-mainText">
+                  See everything the team’s working on in one place.
+                </h1>
+              </div>
+           </div>
+          </div>
         </div>
-        <div className="splash-img">
-         {/* { <img src="assets/teamwork.jpeg" alt="teamwork!"/>} */}
-        </div>
-      </div>
+      </section>
       <Footer />
     </>
   );
