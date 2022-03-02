f7e32dc142e3286b7eaad1f02a0686d5fcfd1149
Rhaubenstock <reedhaubenstock@Reeds-MacBook-Pro.local>
2022-03-01 23:03:00 -0800

    Try to fix heroku migration bug

--- db/migrate/20220302005218_remove_task_unique_constraint.rb
+++ db/migrate/20220302005218_remove_task_unique_constraint.rb
@@ -1,5 +1,8 @@
 class RemoveTaskUniqueConstraint < ActiveRecord::Migration[5.2]
   def change
-     remove_index :tasks,  name: "index_tasks_on_taskable_type_and_taskable_id_and_name"
+    # was originally
+    # remove_index :tasks,  name: "index_tasks_on_taskable_type_and_taskable_id_and_name"
+    # for some reason in heroku db the order of taskable_id and taskable_type were reversed so I changed it below
+     remove_index :tasks,  name: "index_tasks_on_taskable_id_and_taskable_type_and_name"
   end
 end
